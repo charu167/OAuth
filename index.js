2222c0283adf03bf11aa58d1699e2b58ef8f18f0
Charudatta Potdar <charupot08@gmail.com>
2024-02-23 23:03:10 -0700

    temp changes

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const express = require("express");
-require("dotenv").config();
+// const express = require("express");
+// require("dotenv").config();
 
-const authRoutes = require("./routes/auth.routes");
+// const authRoutes = require("./routes/auth.routes");
+
+// const app = express();
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello</h1>");
+// });
+
+// app.use("/auth", authRoutes);
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+const express = require("express");
 
 const app = express();
-app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
-app.use("/auth", authRoutes);
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
