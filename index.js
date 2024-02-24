6e3304aaa41b7c9a32d68134ac2de1deb9844231
Charudatta Potdar <charupot08@gmail.com>
2024-02-23 22:38:12 -0700

    GET route added on homepage

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 
+app.get('/', (req, res)=>{
+  res.send("OAuth backend")
+})
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
