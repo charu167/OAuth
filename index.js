37f90d80572162cd542e2b77132694c1ecbcdeb1
Charudatta Potdar <charupot08@gmail.com>
2024-02-23 23:00:38 -0700

    t

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
-// app.use("/auth", authRoutes);
+app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
