9b3d8f1b7797d9d7127d65f269137a8c9fa20d87
Charudatta Potdar <charupot08@gmail.com>
2024-02-23 23:17:09 -0700

    reverted back index.js

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-// const express = require("express");
-// require("dotenv").config();
-
-// const authRoutes = require("./routes/auth.routes");
-
-// const app = express();
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("<h1>Hello</h1>");
-// });
-
-// app.use("/auth", authRoutes);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
-
 const express = require("express");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`);
+app.use("/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
